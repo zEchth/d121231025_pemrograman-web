e6440601943d1974a340fdbb4269737050797afc
zEchth <fauzanfath099@gmail.com>
2025-09-29 14:52:34 +0800

    change dir

---
+++ portofolio/script.js
@@ -0,0 +1,10 @@
+// let dateNow = new Date().getDay();
+
+// console.log(dateNow);
+
+// getElementById("contact-form").addEventListener("submit", function() {
+//     alert("Form submitted!");
+// });
+
+for(let i=5; i=0; i++){
+}
